1d808a15e29622ba656966529fd5a3415f4a6232
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 00:57:42 +0000

    add Benchmark (cargo) benchmark result for f087fa2b3992d3e5ff9bccac8f02d74f6d9f15b5

---
+++ dev/bench/data.js
@@ -0,0 +1,242 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1771462662079,
+  "repoUrl": "https://github.com/KingingWang/battery-estimator",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kingingwang@foxmail.com",
+            "name": "kingingwang",
+            "username": "KingingWang"
+          },
+          "committer": {
+            "email": "kingingwang@foxmail.com",
+            "name": "kingingwang",
+            "username": "KingingWang"
+          },
+          "distinct": true,
+          "id": "f087fa2b3992d3e5ff9bccac8f02d74f6d9f15b5",
+          "message": "refactor: remove unnecessary implementation comments",
+          "timestamp": "2026-02-19T08:50:48+08:00",
+          "tree_id": "3d351d63d35e5e8d460ade63260f652efb76a393",
+          "url": "https://github.com/KingingWang/battery-estimator/commit/f087fa2b3992d3e5ff9bccac8f02d74f6d9f15b5"
+        },
+        "date": 1771462661247,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "estimate_soc/lipo_3_7v",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc/lifepo4_3_2v",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc/liion_3_7v",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc/conservative_3_77v",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc_fixed",
+            "value": 12,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc_with_temp/cold_temp",
+            "value": 37,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc_with_temp/normal_temp",
+            "value": 38,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc_with_temp/hot_temp",
+            "value": 38,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimate_soc_compensated",
+            "value": 30,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve/voltage_to_soc",
+            "value": 18,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve/voltage_to_soc_fixed",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve/voltage_range",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve_new_small",
+            "value": 24,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve_new_large",
+            "value": 26,
+            "range": "± 2",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "temperature_compensation/default_f32",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "temperature_compensation/default_fixed",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "temperature_compensation/custom_f32",
+            "value": 14,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "temperature_compensation/custom_fixed",
+            "value": 2,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "aging_compensation/f32",
+            "value": 15,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "aging_compensation/fixed",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve_point/new",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve_point/from_fixed",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve_point/voltage",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "curve_point/soc",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimator_creation/new_lipo",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimator_creation/new_lifepo4",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimator_creation/with_temperature_compensation",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimator_creation/with_aging_compensation",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "estimator_creation/with_all_compensation",
+            "value": 1,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "custom_curve_estimate",
+            "value": 18,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "boundary_cases/min_voltage",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "boundary_cases/max_voltage",
+            "value": 23,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "boundary_cases/below_min",
+            "value": 8,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "boundary_cases/above_max",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "throughput_100_estimations",
+            "value": 2425,
+            "range": "± 6",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
